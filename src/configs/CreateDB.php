ebe28f2c8a90bd5f562023395c2fa12f90c955a6
Todor Nikolov <cablio@gmail.com>
2016-04-02 13:01:40 -0700

    Added cookie field to USER and reformatted code.

--- src/configs/CreateDB.php
+++ src/configs/CreateDB.php
@@ -18,8 +18,13 @@ function connect() {
 
 function create_db($mysqli) {
     echo "Creating database HW3.\n";
-    $mysqli->query("DROP DATABASE IF EXISTS HW3;");
+
+    $mysqli->query("DROP DATABASE
+                    IF EXISTS
+                    HW3;");
+
     $mysqli->query("CREATE DATABASE HW3;");
+
     $res = $mysqli->query("USE HW3;");
     if ($res)
         echo "Done.\n\n";
@@ -32,16 +37,20 @@ function create_db($mysqli) {
 
 function create_table_pics($mysqli) {
     echo "Creating table PICTURES.\n";
-    $mysqli->query("DROP TABLE IF EXISTS PICTURES;");
+
+    $mysqli->query("DROP TABLE
+                    IF EXISTS
+                    PICTURES;");
+
     $res = $mysqli->query("CREATE TABLE PICTURES(
-                            IMG_ID INT NOT NULL AUTO_INCREMENT, 
-                            RATING REAL,
-                            USER_ID INT NOT NULL,
-                            CAPTION VARCHAR(255),
-                            POSTED DATE,
-                            PICTURE BLOB(10485760),
-                            PICTURE_LEN INT,
-                            PRIMARY KEY (IMG_ID));");
+                           IMG_ID INT NOT NULL AUTO_INCREMENT, 
+                           RATING REAL,
+                           USER_ID INT NOT NULL,
+                           CAPTION VARCHAR(255),
+                           POSTED DATE,
+                           PICTURE BLOB(10485760),
+                           PICTURE_LEN INT,
+                           PRIMARY KEY (IMG_ID));");
     if ($res)
         echo "Done.\n\n";
     else {
@@ -52,34 +61,37 @@ function create_table_pics($mysqli) {
 }
 
 function populate_table_pics($mysqli) {
-    $captions = [   [],
-                    [0.5, 1, "One is the bluest number."],
-                    [1, 2, "Two on fire."],
-                    [1.5, 3, "Three is deseased."],
-                    [2, 3, "Draw four in under four moves!"],
-                    [2.5, 2, "Cinema."],
-                    [3, 4, "Vintage sign. No filter."],
-                    [3.5, 4, "Rusty 7."],
-                    [4, 1, "8ball"],
-                    [4.5, 4, "Vintage sign. No filter."],
-                    [5, 5, "Noble."] ];
+    $captions = [[],
+                 [0.5, 1, "One is the bluest number."],
+                 [1, 2, "Two on fire."],
+                 [1.5, 3, "Three is deseased."],
+                 [2, 3, "Draw four in under four moves!"],
+                 [2.5, 2, "Cinema."],
+                 [3, 4, "Vintage sign. No filter."],
+                 [3.5, 4, "Rusty 7."],
+                 [4, 1, "8ball"],
+                 [4.5, 4, "Vintage sign. No filter."],
+                 [5, 12, "Noble."] ];
 
     echo "Populating table for pictures.\n";
     $stmt = $mysqli->stmt_init();
-    if ($stmt->prepare("INSERT INTO PICTURES VALUES(?, ?, ?, ?, ?, ?, ?);")) {
+
+    if ($stmt->prepare("INSERT INTO PICTURES
+                        VALUES(?, ?, ?, ?, ?, ?, ?);")) {
+
         for ($i = 1; $i <= 10; $i++) {
             # load file
             $filename = "./init/" . $i . ".jpg";
             $file = file_get_contents($filename);
 
-            $stmt->bind_param(  "idisssi",
-                                $i,                 # id
-                                $captions[$i][0],   # rating
-                                $captions[$i][1],   # user_id
-                                $captions[$i][2],   # caption
-                                date("Y-m-d"),      # date
-                                $file,              # file and size
-                                filesize($filename));
+            $stmt->bind_param("idisssi",
+                              $i,                 # id
+                              $captions[$i][0],   # rating
+                              $captions[$i][1],   # user_id
+                              $captions[$i][2],   # caption
+                              date("Y-m-d"),      # date
+                              $file,              # file and size
+                              filesize($filename));
             $res = $stmt->execute();
             if (!$res) {
                 echo "Failed to populate table PICTURES.\n\n";
@@ -95,13 +107,18 @@ function populate_table_pics($mysqli) {
 
 function create_table_user($mysqli) {
     echo "Creating table USER.\n";
-    $mysqli->query("DROP TABLE IF EXISTS USER;");
+
+    $mysqli->query("DROP TABLE
+                    IF EXISTS
+                    USER;");
+
     $res = $mysqli->query("CREATE TABLE USER(  
-                                USER_ID INT NOT NULL AUTO_INCREMENT,
-                                NAME VARCHAR(100),
-                                USERNAME VARCHAR(20),
-                                PASSWORD VARCHAR(50),
-                                PRIMARY KEY(USER_ID));");
+                           USER_ID INT NOT NULL AUTO_INCREMENT,
+                           NAME VARCHAR(100),
+                           USERNAME VARCHAR(20),
+                           PASSWORD VARCHAR(50),
+                           COOKIE VARCHAR(50),
+                           PRIMARY KEY(USER_ID));");
     if (!$res) {
         echo "Failed to create table USER.\n\n";
         $mysqli->close();
@@ -113,18 +130,22 @@ function create_table_user($mysqli) {
 function populate_table_user($mysqli) {
     echo "Populating table with users.\n";
     $stmt = $mysqli->stmt_init();
-    if ($stmt->prepare("INSERT INTO USER VALUES(?, ?, ?, ?);")) {
-        $users = [  [1, "First User", "fuser", "fpass"],
-                    [2, "Second User", "suser", "spass"],
-                    [3, "Third User", "tuser", "tpass"],
-                    [0, "Zeroth User", "zuser", "zpass"]];
+
+    if ($stmt->prepare("INSERT INTO USER
+                        VALUES(?, ?, ?, ?);")) {
+
+        $users = [[1, "First User", "fuser", "fpass"],
+                  [2, "Second User", "suser", "spass"],
+                  [3, "Third User", "tuser", "tpass"],
+                  [4, "Fourth User", "4user", "4pass"],
+                  [12, "Dozenth User", "duser", "dpass"]];
 
         foreach($users as $user) {
-            $stmt->bind_param(  "isss",
-                                $user[0],   # user_id
-                                $user[1],   # actual name
-                                $user[2],   # username
-                                $user[3]);  # password
+            $stmt->bind_param("isss",
+                              $user[0],   # user_id
+                              $user[1],   # actual name
+                              $user[2],   # username
+                              $user[3]);  # password
             $stmt->execute();
         }
     }
@@ -133,11 +154,16 @@ function populate_table_user($mysqli) {
 
 function create_table_votes($mysqli) {
     echo "Creating table for votes.\n";
-    $mysqli->query("DROP TABLE IF EXISTS VOTES");
-    $r = $mysqli->query("CREATE TABLE VOTES(USER_ID INT NOT NULL,
-                                            IMG_ID INT NOT NULL,
-                                            RATING INT,
-                                            PRIMARY KEY(USER_ID, IMG_ID));");
+
+    $mysqli->query("DROP TABLE
+                    IF EXISTS
+                    VOTES");
+
+    $r = $mysqli->query("CREATE TABLE VOTES(
+                         USER_ID INT NOT NULL,
+                         IMG_ID INT NOT NULL,
+                         RATING INT,
+                         PRIMARY KEY(USER_ID, IMG_ID));");
     if (!$r) {
         echo "Failed to create table VOTES.\n";
         $mysqli->close();
@@ -149,7 +175,10 @@ function create_table_votes($mysqli) {
 function populate_table_votes($mysqli) {
     echo "Populating table VOTES.\n";
     $stmt = $mysqli->stmt_init();
-    if ($stmt->prepare("INSERT INTO VOTES VALUES(?, ?, ?);")) {
+
+    if ($stmt->prepare("INSERT INTO VOTES
+                        VALUES(?, ?, ?);")) {
+
         $ratings = [[1, 1, 1],
                     [2, 2, 2],
                     [3, 3, 3],
@@ -160,8 +189,12 @@ function populate_table_votes($mysqli) {
                     [4, 8, 4.5],
                     [1, 9, 2],
                     [2, 10, 3]];
+
         foreach($ratings as $rating) {
-            $stmt->bind_param('iid', $rating[0], $rating[1], $rating[2]);
+            $stmt->bind_param('iid', 
+                              $rating[0], 
+                              $rating[1], 
+                              $rating[2]);
             $stmt->execute();
         }
     }
