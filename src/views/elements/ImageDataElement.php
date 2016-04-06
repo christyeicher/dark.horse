f8aaae570b6200e713a54acbc06f69c0574904c6
Todor Nikolov <cablio@gmail.com>
2016-04-05 22:40:56 -0700

    More refactoring and prettifying code.

--- src/views/elements/ImageDataElement.php
+++ src/views/elements/ImageDataElement.php
@@ -15,32 +15,57 @@ class ImageDataElement extends Element {
                                 <img class='rating'
                                      src='src/resources/5star.png'
                                      alt='Out of five stars.'
-                                     style='clip:rect(0px "<?php
-                                     echo $data[0][1]*16?>px 16px 0px);'>
+                                     style='clip:rect(0px, <?php
+                                     echo $data[0][1]*16?>px, 16px, 0px);'>
 <?php
-    
 
         // If user hasn't voted, allow voting.
-        if (count($data[1]) and !in_array($data[0][0], $data[1]))
+        if (count($data[1]) and !in_array($data[0][0], $data[1])) {
+            $tabs = "\t\t\t\t";
+            echo $tabs;
             echo "<a href='index.php?nav=vote&vote=5&img=" . $data[0][0] . "'>
-                     <img class='vote' alt='Loved it!' title='Vote: Loved it!'
-                          src='src/resources/novote.png'></a>
-                  <a href='index.php?nav=vote&vote=4&img=" . $data[0][0] . "'>
-                     <img class='vote' alt='Liked it!' title='Vote: Liked it!' 
-                          src='src/resources/novote.png'
-                          style='clip:rect(0px, 64px, 16px, 0px);'></a>
-                  <a href='index.php?nav=vote&vote=3&img=" . $data[0][0] . "'>
-                     <img class='vote' alt='Tis alright!' title='Vote: Tis alright!' 
-                          src='src/resources/novote.png'
-                          style='clip:rect(0px, 48px, 16px, 0px);'></a>
-                  <a href='index.php?nav=vote&vote=2&img=" . $data[0][0] . "'>
-                     <img class='vote' alt='Meh!' title='Vote: Meh!' 
-                          src='src/resources/novote.png'
-                          style='clip:rect(0px, 32px, 16px, 0px);'></a>
-                  <a href='index.php?nav=vote&vote=1&img=" . $data[0][0] . "'>
-                     <img class='vote' alt='Boo!' title='Vote: Boo!' 
-                          src='src/resources/novote.png'
-                          style='clip:rect(0px, 16px, 16px, 0px);'></a>";
+                  \t\t    <img class='vote' 
+                  \t\t         alt='Loved it!' 
+                  \t\t         title='Vote: Loved it!'
+                  \t\t         src='src/resources/novote.png'>
+                  \t\t</a>\n";
+
+            echo $tabs;
+            echo "<a href='index.php?nav=vote&vote=4&img=" . $data[0][0] . "'>
+                  \t\t     <img class='vote' 
+                  \t\t          alt='Liked it!' 
+                  \t\t          title='Vote: Liked it!' 
+                  \t\t          src='src/resources/novote.png'
+                  \t\t          style='clip:rect(0px, 64px, 16px, 0px);'>
+                  \t\t</a>\n";
+
+            echo $tabs;
+            echo "<a href='index.php?nav=vote&vote=3&img=" . $data[0][0] . "'>
+                  \t\t     <img class='vote' 
+                  \t\t          alt='Tis alright!' 
+                  \t\t          title='Vote: Tis alright!' 
+                  \t\t          src='src/resources/novote.png'
+                  \t\t          style='clip:rect(0px, 48px, 16px, 0px);'>
+                  \t\t</a>\n";
+
+            echo $tabs;
+            echo "<a href='index.php?nav=vote&vote=2&img=" . $data[0][0] . "'>
+                  \t\t     <img class='vote' 
+                  \t\t          alt='Meh!' 
+                  \t\t          title='Vote: Meh!' 
+                  \t\t          src='src/resources/novote.png'
+                  \t\t          style='clip:rect(0px, 32px, 16px, 0px);'>
+                  \t\t</a>\n";
+
+            echo $tabs;
+            echo "<a href='index.php?nav=vote&vote=1&img=" . $data[0][0] . "'>
+                 \t\t      <img class='vote' 
+                 \t\t           alt='Boo!' 
+                 \t\t           title='Vote: Boo!' 
+                 \t\t           src='src/resources/novote.png'
+                 \t\t           style='clip:rect(0px, 16px, 16px, 0px);'>
+                 \t\t</a>\n";
+        }
 ?>
                             </span><br>
                             Date: " <?php echo $data[0][4]?><br>
